d1cecadcf83978592cde02ffef1720c1eea64786
Puo Syer Ang <puosyerang@Puos-MacBook-Air.local>
2022-08-26 03:31:28 +0800

    feat: finish component logic for artwork

--- components/cert.tsx
+++ components/cert.tsx
@@ -1,10 +1,14 @@
-
-
-export default function Cert({ transaction }) {
-    return (
-        <div>
-            <h1>Proud Owner of artwork {transaction.artwork.label}</h1>
-            <h2>By someone {transaction.to.username}</h2>
-        </div>
-    );
+export default function Cert({ transaction, artwork }) {
+  console.log(artwork);
+  console.log(transaction);
+  return (
+    <div>
+      {transaction && artwork && (
+        <>
+          <h1>Proud Owner of artwork {artwork.label}</h1>
+          <h2>By someone {transaction.to.username}</h2>
+        </>
+      )}
+    </div>
+  );
 }
--- pages/artwork/[id].tsx
+++ pages/artwork/[id].tsx
@@ -1,50 +1,53 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import useSWR from 'swr';
-import Cert from '../../components/cert';
+import Head from "next/head";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Cert from "../../components/cert";
 
-import Layout from '../../components/layout';
-import fetchJson from '../../lib/fetchJson';
-import utilStyles from '../../styles/utils.module.css';
+import Layout from "../../components/layout";
+import fetchJson from "../../lib/fetchJson";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Artwork() {
-    const router = useRouter();
-    const { id } = router.query;
+  const router = useRouter();
+  const { id } = router.query;
 
-    const { data: artwork, error } = useSWR<any>(`http://localhost:8000/artwork/${id}`, (url) => {
-        console.log(url)
-        return fetch(url).then(x => x.json)
-    });
-    const { data: transaction } = useSWR<any>(`http://localhost:8000/request/cert/${id}`, fetchJson);
-
-    console.log(artwork);
-    console.log(transaction);
-    if (error) console.log(error);
-    return (
-        <Layout>
-            <Head>
-                <title>BlockchainTS</title>
-            </Head>
-            {artwork && <div className={utilStyles.artworkContainer}>
-                <div className={utilStyles.artworkItem}>
-                    <div className={utilStyles.artworkImage}>
-                        <Image
-                            priority
-                            src={artwork.artworkPath}
-                            width={1024}
-                            height={1024}
-                            alt={artwork.label}
-                        />
-                    </div>
-                    <h2>{artwork.label}</h2>
-                    <p>by {artwork.artist?.username}</p>
-                    <br />
-                    <p>Owned by: {artwork.owner?.username}</p>
-                    <button>Request to Buy</button>
-                </div>
-            </div>}
-            {transaction && <Cert transaction={transaction}></Cert>}
-        </Layout>
-    );
+  const { data: artwork, error } = useSWR<any>(
+    `http://localhost:8000/artwork/${id}`,
+    fetchJson
+  );
+  const {
+    data: { data: transaction },
+  } = useSWR<any>(`http://localhost:8000/request/cert/${id}`, fetchJson);
+  console.log(transaction);
+  if (error) console.log(error);
+  return (
+    <Layout>
+      <Head>
+        <title>BlockchainTS</title>
+      </Head>
+      {artwork && (
+        <div className={utilStyles.artworkContainer}>
+          <div className={utilStyles.artworkItem}>
+            <div className={utilStyles.artworkImage}>
+              <Image
+                priority
+                loader={() => artwork.artworkPath}
+                src={artwork.artworkPath}
+                width={1024}
+                height={1024}
+                alt={artwork.label}
+              />
+            </div>
+            <h2>{artwork.label}</h2>
+            <p>by {artwork.artist?.username}</p>
+            <br />
+            <p>Owned by: {artwork.owner?.username}</p>
+            <button>Request to Buy</button>
+          </div>
+        </div>
+      )}
+      {transaction && <Cert transaction={transaction} artwork={artwork}></Cert>}
+    </Layout>
+  );
 }
